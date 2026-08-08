afca678c0a9ee6b35b8a6b03e44ada69f90feeab
Muaz Naeem <muaznaeem841@gmail.com>
2026-08-08 20:06:34 +0500

    board will store board and 2 additional methods to validate and iterate pieces
    piece will store piece model from input-parser

---
+++ Lights-out/Lights-out/Models/Boards.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lights_out.Models;
+
+public  class Board
+{
+    private readonly int[,] _cells;
+    private readonly int _depth;
+
+    public int Rows { get; }
+    public int Cols { get; }
+
+    public Board(int[,] cells, int depth)
+    {
+        _cells = cells;
+        _depth = depth;
+        Rows = cells.GetLength(0);
+        Cols = cells.GetLength(1);
+    }
+
+    public bool IsSolved()
+    {
+        foreach (var value in _cells)
+        {
+            if (value != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public void Apply(Piece piece, int x, int y, int amount)
+    {
+        for (int pr = 0; pr < piece.Rows; pr++)
+        {
+            for (int pc = 0; pc < piece.Cols; pc++)
+            {
+                if (piece.Cells[pr, pc])
+                {
+                    int row = y + pr;
+                    int col = x + pc;
+                    _cells[row, col] = (_cells[row, col] + amount) % _depth;
+                }
+            }
+        }
+    }
+}
---
+++ Lights-out/Lights-out/Models/Piece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lights_out.Models;
+
+public class Piece
+{
+    public bool[,] Cells { get; }
+    public int Rows { get; }
+    public int Cols { get; }
+
+    public Piece(bool[,] cells)
+    {
+        Cells = cells;
+        Rows = cells.GetLength(0);
+        Cols = cells.GetLength(1);
+    }
+}
